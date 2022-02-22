3250dc5f5a384ae01b12b9d1cfffe5f295494b8e
Brooke Wilder <brookewilder19@gmail.com>
2022-02-22 09:54:30 -0500

    added seed file

---
+++ seed.js
@@ -0,0 +1,23 @@
+const db = require('./server/db');
+// require models here
+
+//find way to get data, maybe in array and Promise all with create?
+
+const main = () => {
+    console.log('Syncing db...');
+    db.sync({ force: true })
+      .then(() => {
+        console.log('Seeding databse...');
+        return seed();
+      })
+      .catch(err => {
+        console.log('Error while seeding');
+        console.log(err.stack);
+      })
+      .then(() => {
+        db.close();
+        return null;
+      });
+  };
+  
+  main();
