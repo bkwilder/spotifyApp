e0e172da997f7a3862ff81adb46be7f6ee93e943
Brooke Wilder <brookewilder19@gmail.com>
2022-02-28 09:59:00 -0500

    Merge branch 'main' of github.com:bkwilder/spotifyApp

--- client/components/Authorization.js
+++ client/components/Authorization.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import { connect } from 'react-redux'
 import {setTracks, loginView} from '../store'
 
-class Authorization extends Component{
+export default class Authorization extends Component{
     constructor() {
         super();
         this.requestAuthorization = this.requestAuthorization.bind(this)
@@ -10,25 +10,15 @@ class Authorization extends Component{
 
     requestAuthorization(){
         window.location.href = 'http://localhost:8080/login'
-        this.props.changeLogin();
     }
 
     render() {
         return (
             <div className='welcome'>
-                <h1>What are your top listened to genres in Spotify?</h1>
+                <h1 className='heading'>What are your top listened to genres in Spotify?</h1>
                 <button className='login' id='authorization' onClick={this.requestAuthorization}>Click here to find out!</button>
             </div>
         )
     }
 }
 
-function mapDispatchToProps(dispatch){
-    return {
-        setTracks: () => dispatch(setTracks()),
-        changeLogin: () => dispatch(loginView())
-    }
-}
-
-   
-   export default connect(null, mapDispatchToProps)(Authorization)
